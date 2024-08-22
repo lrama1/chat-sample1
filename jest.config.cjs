bfdbdf77a4c585cf07810b1dd316c5b2104c220c
Lawrence Rama <oleng_rama@hotmail.com>
2024-08-22 19:46:18 -0400

    fix: relocated and fixed the unit tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,4 +8,5 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 };
--- src/components/__tests__/Chat.test.jsx
+++ src/components/__tests__/Chat.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Chat from './Chat';
+import Chat from '../Chat';
 
 test('initially displays the chat icon', () => {
   render(<Chat />);
